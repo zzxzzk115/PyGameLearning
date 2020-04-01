2bf7e700bebcfb8c112e338f347de5d44c2d20e9
Lazy_V <33739170+zzxzzk115@users.noreply.github.com>
2020-04-01 13:00:04 +0800

    up to date

---
+++ 02-Show Your First Sprite/README.md
@@ -0,0 +1,19 @@
+# 01-Your First Game Window
+**Screenshot**:
+
+![first window](https://github.com/zzxzzk115/PyGameLearning/raw/master/screenshots/first-sprite.png)
+
+
+
+Import`os` first.
+
+Use `os.path.dirname(__file__)` to get the name of the game's root directory.
+
+Use `os.path.join($pathA, $pathB)` to join the path : `pathA/pathB`.
+
+Use `pygame.image.load(path)` to load a image, use `convert_alpha()` to convert the image into alpha-support mode.
+
+
+
+
+
--- 02-Show Your First Sprite/main.py
+++ 02-Show Your First Sprite/main.py
@@ -4,23 +4,41 @@
 @Author: JackZhang
 @Date: 2020-03-31 20:48:47
 @LastEditors: JackZhang
-@LastEditTime: 2020-04-01 12:17:57
+@LastEditTime: 2020-04-01 12:12:08
 '''
 # import module.
-import pygame, sys
+import pygame, sys, os
 from pygame import Color
 
+# set-up path
+game_path = os.path.dirname(__file__)
+img_path = os.path.join(game_path, "img")
+# print(game_path, img_path)
+
 # set up custom game window size.
-window_size = width, height = 400, 300
+window_size = width, height = 336, 607
 
 # initialize the pygame and create the main window.
 pygame.init()
-pygame.display.set_caption("01-Your First Game Window")
+pygame.display.set_caption("02-Show Your First Sprite")
 screen = pygame.display.set_mode(window_size)
 
 # set frame-per-second and the clock.
 fps = 30
 clock = pygame.time.Clock()
+
+# class Player extends class pygame.sprite.Sprite
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join(img_path, "player.png")).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.center = (width/2, height/2)
+
+# you should add all the sprites you want to show on the screen to the pygame.sprite.Group.
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 
 # a flag.
 running = True
@@ -39,10 +57,11 @@ while running:
                 running = False
 
     # update
-
+    all_sprites.update()
     # draw and render
-    screen.fill(Color("yellow"))
-    
+    screen.fill(Color("grey"))
+    all_sprites.draw(screen)
+
     # flush screen.
     pygame.display.flip()
     # or pygame.display.update()
